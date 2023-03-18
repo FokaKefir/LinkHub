f3b28ca130a5d4e4856ccbb1c17f641408214d8e
SikoSzabolcs17 <sikoszabolcs17@yahoo.com>
2023-03-18 17:35:39 +0200

    PlaceAdapter v.02

--- app/src/main/java/com/fokakefir/linkhub/gui/fragment/SearchFragment.java
+++ app/src/main/java/com/fokakefir/linkhub/gui/fragment/SearchFragment.java
@@ -9,6 +9,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.fokakefir.linkhub.R;
 import com.fokakefir.linkhub.gui.activity.MainActivity;
@@ -18,11 +21,13 @@ import com.fokakefir.linkhub.model.Place;
 import java.util.ArrayList;
 
 
-public class SearchFragment extends Fragment {
+public class SearchFragment extends Fragment  implements View.OnClickListener{
 
     private MainActivity activity;
 
     private RecyclerView recyclerView;
+    private EditText txtSearch;
+    private Button btnSearch;
     private PlaceAdapter adapter;
     private RecyclerView.LayoutManager layoutManager;
 
@@ -33,9 +38,12 @@ public class SearchFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)  {
         this.view = inflater.inflate(R.layout.fragment_search, container, false);
+        this.txtSearch = this.view.findViewById(R.id.txt_frag_search);
+        this.btnSearch = this.view.findViewById(R.id.btn_frag_search);
 
+        btnSearch.setOnClickListener(this);
 
         ArrayList<Place> places = new ArrayList<>();
 
@@ -51,5 +59,12 @@ public class SearchFragment extends Fragment {
 
 
         return this.view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btn_frag_search){
+            Toast.makeText(activity, this.txtSearch.getText(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
